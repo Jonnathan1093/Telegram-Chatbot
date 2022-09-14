c5b775d1969654a49559303a16e89c1823c6c3a8
Jonnathan Heras <jonnathan1093@yahoo.com>
2022-09-14 12:01:46 -0500

    Desplegar lista de comandos /

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-TELEGRAM_TOKEN = "5760496005:AAGl6kjIxka8ZrQR9Uqxyc2kX9ggzHtPtqk"
-
 # Este es mi bot creado -->  RetroGeeksBot
 
+TELEGRAM_TOKEN = "5760496005:AAGl6kjIxka8ZrQR9Uqxyc2kX9ggzHtPtqk"
+MI_CHAT_ID = 374410616 # Este es mi chat_id con el bot
+CANAL_ID = -1001785246267 # Id del canal 
--- test_telegram.py
+++ test_telegram.py
@@ -1,6 +1,7 @@
 from config import * #Importamos el token
 import telebot #para manejar la API de telegram
 import time
+import threading
 # Instanciamos el bot de Telegram
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
@@ -14,8 +15,10 @@ bot = telebot.TeleBot(TELEGRAM_TOKEN)
 def cmd_start(message):
     # Da la bienvenida al usuario del bot
     bot.reply_to(message, "Hola, aqui encontraras todo lo que te gusta")
+    print(message.chat.id)
 
 @bot.message_handler(content_types=["text"])
+# @bot.message_handler(content_types=["text", "photo"]) # Se ejecutata si recibe texto o imagem
 # Responde a los mensajes de texto que no son comandos o se puede usar los de abajo dependiendo
 # text, audio, document, photo, sticker, video, video_note, 
 # voice, location, contact, new_chat_members, left_chat_member, 
@@ -45,24 +48,54 @@ def bot_mensajes_texto(message):
 #     texto_markdown+= '[ENLACE](https://retrogeeksla.blogspot.com/)' + '\n' # Enlace
 
 # Gestionar los mensajes de texto recibidos    
-    if message.text.startswith("/"): #Controlo el texto para que no inicien con barra
+    if message.text and message.text.startswith("/"): #Controlo el texto para que no inicien con barra
         bot.send_message(message.chat.id, "Comando no disponible") #Controla el texto si por ejemplo ponen: /hola 
     else:
-        x = bot.send_message(message.chat.id, "<b>Hola</b>", parse_mode="html", disable_web_page_preview=True)
-        time.sleep(3) # Pausa
+        # x = bot.send_message(message.chat.id, "<b>Hola</b>", parse_mode="html", disable_web_page_preview=True)
+        # time.sleep(3) # Pausa
         # bot.edit_message_text("<u>Adios</u>", message.chat.id, x.message_id, parse_mode="html") # Para editar el mensaje, como primer argumento recibe el mensaje, y segundo el identificador
         # bot.delete_message(message.chat.id, x.message_id) # Eliminar mensaje bot
-        bot.delete_message(message.chat.id, message.message_id)
+        # bot.delete_message(message.chat.id, message.message_id)
         # bot.send_message(message.chat.id, texto_html, parse_mode="html", disable_web_page_preview=True) 
         # bot.send_message(message.chat.id, texto_markdown, parse_mode="MarkdownV2", disable_web_page_preview=True)  # Podemos ver en formato Markdown    
         # De esta manera obtenemos el chat id del usuario que nos envio el mensaje parse_mode me permite enviar en formato html
         # Para controlar la previsualizacion de nuestro enlace usamos disable_web_page_preview, el valor sera true, porque por defecto es false
-
+        
+        # # Primer argumento chat id, segundo el objeto que contiene la foto, el tercero es opcional, es un "Caption" puede ir un mensaje
+        # foto = open("./Imagenes/Marca de Maldicion.jpg", "rb")
+        # bot.send_photo(message.chat.id, foto, "Marca") 
+        
+        # # Para enviar un archivo
+        # archivo = open("./Docs/Problemas de geometria analitica plana.pdf", "rb")
+        # bot.send_document(message.chat.id, archivo, caption="Libro Geometria")
+        
+        # Para mostrar en la barra superior escribiendo
+        bot.send_chat_action(message.chat.id, "upload_video")
+        # action_string can be one of the following strings: 'typing', 'upload_photo', 'record_video', 'upload_video',
+        # 'record_audio', 'upload_audio', 'upload_document' or 'find_location'
+        
+        # Para enviar un video
+        video = open("./Video/Otoko Intro.mp4", "rb")
+        bot.send_video(message.chat.id, video, caption="Mira mi intro")
+      
+def recibir_mensajes():
+    bot.infinity_polling()
+    
 # Comenzamos a escribir nuestro programa principal
 if __name__== '__main__': # Para indicar que se inicia el bot
+    # Este recibira los argumentos, el primero sera el comando, y el segundo la descripcion del mmismo
+    bot.set_my_commands([
+        telebot.types.BotCommand("/start", "Inicia el bot"), 
+        telebot.types.BotCommand("/boom", "Explota el mundo")
+        ])
     print('Iniciando el bot') 
-    bot.infinity_polling() # Este metodo se trata de un bucle infinito, comprueba constante mente si se reciben mensajes nuevos
-    print('Fin')
+    hilo_bot = threading.Thread(name="hilo_bot", target=recibir_mensajes)
+    hilo_bot.start() # Con esto iniciamos
+    # bot.infinity_polling() # Este metodo se trata de un bucle infinito, comprueba constantemente si se reciben mensajes nuevos
+    print('Bot iniciado')
+    # bot.send_message(MI_CHAT_ID, "Python mola mucho") # Mandamos al mismo bot
+    bot.send_message(CANAL_ID, "Python mola mucho") # Mandamos al canal
+    
 
 
 
